ed4e86728ce338a9c6135205c5641beb06aca530
Sofiannedeer <ojingirihannah22@gmail.com>
2023-10-31 05:45:03 -0700

    python loop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) is not '101' and chr(letter) is not '113':
-        print(chr(letter).format(), end='''')
+if chr(letter) is not '101' and chr(letter) is not '113':
+continue
+print(chr(letter).format(), end="")
